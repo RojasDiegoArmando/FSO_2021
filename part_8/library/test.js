1fb1829a0b5f27fe0a1a6b506dfad366393dddc1
rojasdiegoarmando <diegorealmadrid7@gmail.com>
2022-08-20 19:35:44 -0300

    part_8 stated, execise 8.1: The number of books and authors finished

---
+++ part_8/library/test.js
@@ -0,0 +1,79 @@
+let books = [
+    {
+        title: 'Clean Code',
+        published: 2008,
+        author: 'Robert Martin',
+        id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
+        genres: ['refactoring']
+    },
+    {
+        title: 'Agile software development',
+        published: 2002,
+        author: 'Robert Martin',
+        id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
+        genres: ['agile', 'patterns', 'design']
+    },
+    {
+        title: 'Refactoring, edition 2',
+        published: 2018,
+        author: 'Martin Fowler',
+        id: "afa5de00-344d-11e9-a414-719c6709cf3e",
+        genres: ['refactoring']
+    },
+    {
+        title: 'Refactoring to patterns',
+        published: 2008,
+        author: 'Joshua Kerievsky',
+        id: "afa5de01-344d-11e9-a414-719c6709cf3e",
+        genres: ['refactoring', 'patterns']
+    },
+    {
+        title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
+        published: 2012,
+        author: 'Sandi Metz',
+        id: "afa5de02-344d-11e9-a414-719c6709cf3e",
+        genres: ['refactoring', 'design']
+    },
+    {
+        title: 'Crime and punishment',
+        published: 1866,
+        author: 'Fyodor Dostoevsky',
+        id: "afa5de03-344d-11e9-a414-719c6709cf3e",
+        genres: ['classic', 'crime']
+    },
+    {
+        title: 'The Demon ',
+        published: 1872,
+        author: 'Fyodor Dostoevsky',
+        id: "afa5de04-344d-11e9-a414-719c6709cf3e",
+        genres: ['classic', 'revolution']
+    },
+]
+
+let blogsByAuthorMap = books.reduce((acc, curr) => {
+    const name = curr.author
+    if (acc.has(name)) {
+        acc.set(name, { name, blogs: acc.get(name).books + 1 })
+    } else {
+        acc.set(name, { name, books: 1 })
+    }
+    return acc
+}, new Map())
+
+
+const blogsByAuthor = [...blogsByAuthorMap.values()]
+console.log(blogsByAuthorMap.size)
+console.log(blogsByAuthor)
+
+let nmap = new Map()
+books.forEach(book => {
+    const name = book.author
+    if (nmap.has(name)) {
+        nmap.set(name, { name, books: nmap.get(name).books + 1 })
+    } else {
+        nmap.set(name, { name, books: 1 })
+    }
+    return nmap
+})
+
+console.log(nmap.size)
